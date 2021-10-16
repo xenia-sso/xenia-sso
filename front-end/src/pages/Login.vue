4102cb2527fd866683101f5997cda103df6389e2
Gaetan Chambouvet <gaetan.chambouvet@gmail.com>
2021-10-16 16:08:47 +0200

    Add form validation to login page

--- front-end/src/pages/Login.vue
+++ front-end/src/pages/Login.vue
@@ -1,23 +1,60 @@
 <template>
-  <div class="row q-col-gutter-sm">
-    <div class="col col-12 text-h6">Login</div>
+  <q-form @submit="submit">
+    <div class="row q-col-gutter-sm">
+      <div class="col col-12 text-h6">Login</div>
 
-    <div class="col col-12">
-      <q-input filled square dense label="Email"></q-input>
-    </div>
+      <div class="col col-12">
+        <q-input
+          v-model="formFields.email"
+          filled
+          square
+          dense
+          label="Email"
+          lazy-rules
+          :rules="[RULES.required, RULES.email]"
+        ></q-input>
+      </div>
 
-    <div class="col col-12">
-      <q-input filled square dense type="password" label="Password"></q-input>
-    </div>
+      <div class="col col-12">
+        <q-input
+          v-model="formFields.password"
+          filled
+          square
+          dense
+          type="password"
+          label="Password"
+          lazy-rules
+          :rules="[RULES.required]"
+        ></q-input>
+      </div>
 
-    <div class="col col-12 q-mt-sm">
-      <q-btn color="primary" class="full-width">Continue</q-btn>
+      <div class="col col-12 q-mt-sm">
+        <q-btn type="submit" color="primary" class="full-width">Continue</q-btn>
+      </div>
     </div>
-  </div>
+  </q-form>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref } from 'vue';
+import { RULES } from 'src/utils/form-validation';
 
-export default defineComponent({});
+export default defineComponent({
+  setup() {
+    const submit = () => {
+      console.log('submit');
+    };
+
+    const formFields = ref({
+      email: '',
+      password: '',
+    });
+
+    return {
+      RULES,
+      formFields,
+      submit,
+    };
+  },
+});
 </script>
