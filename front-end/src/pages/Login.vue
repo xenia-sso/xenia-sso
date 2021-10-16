253bf7ed2cb7d8d2ce34e2967f24717298333abc
Gaetan Chambouvet <gaetan.chambouvet@gmail.com>
2021-10-16 15:23:55 +0200

    Improve login page structure

--- front-end/src/pages/Login.vue
+++ front-end/src/pages/Login.vue
@@ -1,8 +1,19 @@
 <template>
-  <div class="text-h6">Login</div>
-  <q-input filled square dense label="Email" class="q-mt-sm"></q-input>
-  <q-input filled square dense type="password" label="Password" class="q-mt-sm"></q-input>
-  <q-btn color="primary" class="full-width q-mt-md">Continue</q-btn>
+  <div class="row q-col-gutter-sm">
+    <div class="col col-12 text-h6">Login</div>
+
+    <div class="col col-12">
+      <q-input filled square dense label="Email"></q-input>
+    </div>
+
+    <div class="col col-12">
+      <q-input filled square dense type="password" label="Password"></q-input>
+    </div>
+
+    <div class="col col-12 q-mt-sm">
+      <q-btn color="primary" class="full-width">Continue</q-btn>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
