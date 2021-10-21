2f5ad9149dcf199f31f2c5982ac3e43db5976e97
Gaetan Chambouvet <gaetan.chambouvet@gmail.com>
2021-10-21 18:25:07 +0200

    Init Ts.ED project

---
+++ back-end/README.md
@@ -0,0 +1,21 @@
+# back-end
+
+> An awesome project based on Ts.ED framework
+
+See [Ts.ED](https://tsed.io) project for more information.
+
+## Build setup
+
+> **Important!** Ts.ED requires Node >= 10, Express >= 4 and TypeScript >= 3.
+
+```batch
+# install dependencies
+$ npm install
+
+# serve
+$ npm run start
+
+# build for production
+$ npm run build
+$ npm run start:prod
+```
---
+++ back-end/src/server.ts
@@ -0,0 +1,48 @@
+import { Configuration, Inject } from "@tsed/di";
+import { PlatformApplication } from "@tsed/common";
+import "@tsed/platform-express"; // /!\ keep this import
+import bodyParser from "body-parser";
+import compress from "compression";
+import cookieParser from "cookie-parser";
+import methodOverride from "method-override";
+import cors from "cors";
+import "@tsed/ajv";
+import { config, rootDir } from "./config";
+
+@Configuration({
+  ...config,
+  acceptMimes: ["application/json"],
+  httpPort: process.env.PORT || 8083,
+  httpsPort: false, // CHANGE
+  mount: {
+    "/rest": [`${rootDir}/controllers/**/*.ts`],
+  },
+  views: {
+    root: `${rootDir}/views`,
+    extensions: {
+      ejs: "ejs",
+    },
+  },
+  exclude: ["**/*.spec.ts"],
+})
+export class Server {
+  @Inject()
+  app: PlatformApplication;
+
+  @Configuration()
+  settings: Configuration;
+
+  $beforeRoutesInit(): void {
+    this.app
+      .use(cors())
+      .use(cookieParser())
+      .use(compress({}))
+      .use(methodOverride())
+      .use(bodyParser.json())
+      .use(
+        bodyParser.urlencoded({
+          extended: true,
+        })
+      );
+  }
+}
