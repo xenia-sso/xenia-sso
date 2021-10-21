f50564b2cedd787f5c876d1ec34bec33066ef8f8
Gaetan Chambouvet <gaetan.chambouvet@gmail.com>
2021-10-21 18:27:09 +0200

    Clear unnecessary deps

--- back-end/src/server.ts
+++ back-end/src/server.ts
@@ -1,11 +1,10 @@
 import { Configuration, Inject } from "@tsed/di";
 import { PlatformApplication } from "@tsed/common";
 import "@tsed/platform-express"; // /!\ keep this import
-import bodyParser from "body-parser";
 import compress from "compression";
 import cookieParser from "cookie-parser";
 import methodOverride from "method-override";
-import cors from "cors";
+import express from "express";
 import "@tsed/ajv";
 import { config, rootDir } from "./config";
 
@@ -33,16 +32,6 @@ export class Server {
   settings: Configuration;
 
   $beforeRoutesInit(): void {
-    this.app
-      .use(cors())
-      .use(cookieParser())
-      .use(compress({}))
-      .use(methodOverride())
-      .use(bodyParser.json())
-      .use(
-        bodyParser.urlencoded({
-          extended: true,
-        })
-      );
+    this.app.use(cookieParser()).use(compress({})).use(methodOverride()).use(express.json());
   }
 }
