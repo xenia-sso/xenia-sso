6ccfbcd0908df65307d4348bb5b7aa49f7fdc3ce
Gaetan Chambouvet <gaetan.chambouvet@gmail.com>
2021-10-27 19:02:32 +0200

    Create clients model

---
+++ back-end/src/models/client.model.ts
@@ -0,0 +1,27 @@
+import { OnDeserialize } from "@tsed/json-mapper";
+import { Model, ObjectID, Ref } from "@tsed/mongoose";
+import { Required } from "@tsed/schema";
+import { UserModel } from "./user.model";
+
+@Model({ collection: "clients" })
+export class ClientModel {
+  // Workaround to prevent passing "id" in incoming requests
+  @OnDeserialize((v) => {
+    if (typeof v === "string") {
+      return undefined;
+    }
+    return v;
+  })
+  @ObjectID("id")
+  _id: string;
+
+  @Required()
+  name: string;
+
+  @Required()
+  allUsers: boolean;
+
+  @Required()
+  @Ref(UserModel)
+  grantedUsers: Ref<UserModel>[];
+}
