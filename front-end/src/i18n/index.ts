481de987362b742efa8e6146fbfe72e59dd12f9c
Gaetan Chambouvet <gaetan.chambouvet@gmail.com>
2021-11-22 21:46:57 +0100

    Add i18n to page titles

--- front-end/src/i18n/index.ts
+++ front-end/src/i18n/index.ts
@@ -1,14 +1,32 @@
 export default {
   en: {
+    // form validation
     fieldRequired: 'This field is required.',
     invalidEmail: 'Invalid email.',
     invalidPassword: 'Should contain at least 6 characters.',
     invalidUrl: 'Invalid URL.',
+    // page titles
+    pageTitles: {
+      myProfile: 'My Profile',
+      myApps: 'My Apps',
+      clients: 'Clients',
+      users: 'Users',
+      invitationCodes: 'Invitation codes',
+    },
   },
   fr: {
+    // form validation
     fieldRequired: 'Ce champ est requis.',
     invalidEmail: 'Email invalide.',
     invalidPassword: 'Doit contenir au moins 6 caractères.',
     invalidUrl: 'URL invalide.',
+    // page titles
+    pageTitles: {
+      myProfile: 'Mon Profil',
+      myApps: 'Mes Applis',
+      clients: 'Clients',
+      users: 'Utilisateurs',
+      invitationCodes: "Codes d'invitation",
+    },
   },
 };
