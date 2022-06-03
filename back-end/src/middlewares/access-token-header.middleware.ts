1f00e3d677a0f1c5aa9cf53d2cd64e22819171ed
Gaetan Chambouvet <gaetan.chambouvet@gmail.com>
2022-06-03 18:50:03 +0200

    Code cleanup

--- back-end/src/middlewares/access-token-header.middleware.ts
+++ back-end/src/middlewares/access-token-header.middleware.ts
@@ -4,7 +4,7 @@ import { AccessTokenMiddleware } from "./utils/access-token";
 @Middleware()
 export class AccessTokenHeaderMiddleware extends AccessTokenMiddleware {
   async use(@Context() ctx: Context, @Req() req: Req, @Res() res: Res, @Next() next: Next) {
-    const { user, token } = await this.getUserFromToken(req.headers["authorization"]);
+    const { user, token } = await this.getUserFromToken(req.headers.authorization);
 
     ctx.set("user", user);
     ctx.set("token", token);
